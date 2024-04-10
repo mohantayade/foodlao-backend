2395e80718a8303bd32b8542b1754386fbc76712
mohantayade <mohantayade326@gmail.com>
2024-04-10 13:30:01 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express,{Request,Response}  from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute"
+
+
+
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(()=> console.log("Connect to database!"))
+
+
+const app = express();
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/health", async (req: Request,res: Response)=>{
+    res.send({message: "Health ok!"})
+})
+
+app.use("/api/my/user", myUserRoute)
+
+
+app.listen(7000,()=>{
+    console.log("server start Localhost:7000")
+})
